58973e274b5f1f0f06444ce43e395e9f6f5e55f2
zuorui-code <17775435939@163.com>
2020-10-12 18:20:56 +0800

    insert eureka

---
+++ 04-eureka/src/main/java/com/zr/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.zr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zuorui
+ * @creat 2020-10-12-14:17
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
---
+++ 04-eureka/src/main/java/com/zr/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.zr.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zuorui
+ * @creat 2020-10-12-17:14
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
